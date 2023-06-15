77306235982b8763889db591e39d93d63835acc8
Lumi <shaicloud@gmail.com>
2023-06-15 19:56:14 -0400

    fixed footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,26 @@
 import "../styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="Footer">
-            <p>
-                Built by Lumi&nbsp;&nbsp;&nbsp;
-                <a
-                    href="https://github.com/Lumi-sg/ShoppingCart"
-                    style={{ textDecoration: "none" }}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i style={{ fontSize: 24 }} className="fa">
-                        &#xf09b;
-                    </i>
-                </a>
-            </p>
-        </footer>
-    );
+	return (
+		<footer className="Footer">
+			<p>
+				Built by Lumi&nbsp;&nbsp;&nbsp;
+				<a
+					href="https://github.com/Lumi-sg/WheresWaldo"
+					style={{ textDecoration: "none" }}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i
+						style={{ fontSize: 24 }}
+						className="fa"
+					>
+						&#xf09b;
+					</i>
+				</a>
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
